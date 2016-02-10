b455b7a26f4f17e3a75916556d8af8aae4fed491
Radek Los <radek.los@gmail.com>
2016-02-10 13:45:45 +0000

    Set keystore password

---
+++ ci/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# decrypt signatures
+openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in keys/fm.jks.enc -out fm.jks
+# openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in ci/play-store.p12.enc -out play-store.p12
