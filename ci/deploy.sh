1da55d8ff362191a6994f73d5d81c27ff901adb2
Radek Los <radek.los@gmail.com>
2016-02-25 10:25:35 +0000

    p12 key

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,4 +2,4 @@
 
 # decrypt signatures
 openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in keys/fm.jks.enc -out fm.jks
-# openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in ci/play-store.p12.enc -out play-store.p12
+openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in keys/play-store.p12.enc -out play-store.p12
