801e0755932ba50c25358577505c6b7584ca3062
Radek Los <radek.los@gmail.com>
2016-02-25 11:09:56 +0000

    Release apk

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,3 +3,5 @@
 # decrypt signatures
 openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in keys/fm.jks.enc -out fm.jks
 openssl aes-256-cbc -d -k "$FILE_PASSWORD" -in keys/play-store.p12.enc -out play-store.p12
+
+gradle publishApkRelease
